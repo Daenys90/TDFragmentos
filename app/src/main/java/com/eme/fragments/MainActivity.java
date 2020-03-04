27ed30bb62128ef30b1315406c3b6594d3b12517
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2020-03-04 12:07:03 -0300

    primer commit proyecto con 2 fragmentos en un activity

---
+++ app/src/main/java/com/eme/fragments/MainActivity.java
@@ -0,0 +1,55 @@
+package com.eme.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonF1;
+    Button buttonF2;
+    BlankFragment f = BlankFragment.newInstance("Hola","Chao");
+    BlankFragment2 f2 = BlankFragment2.newInstance("Hola","Chao");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //buttonF1 = findViewById(R.id.myTextView);
+        //buttonF2 = findViewById(R.id.button2);
+        init();
+    }
+
+
+    private void init() {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.con_tenedor, f, "BLANK")
+                .add(R.id.con_cuchillo, f2, "BLANK2")
+                .commit();
+
+
+        /*
+        buttonF1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("bla", "creando fragmento");
+
+                getSupportFragmentManager().beginTransaction().add(R.id.con_tenedor, f, "BLANK").commit();
+
+            }
+        });
+
+        buttonF2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("bla", "creando fragmento");
+
+                getSupportFragmentManager().beginTransaction().add(R.id.con_cuchillo, f2, "BLANK2").commit();
+            }
+        });*/
+    }
+}
